6098783535c12f0ca0ca7c837bd3290b10cd3eb5
Tyzzko <70973519+Tyzzko@users.noreply.github.com>
2021-01-12 22:59:29 +0300

    woooorld

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from random import randrange
 
 WIDTH, HEIGHT = 1211, 723
 fps = 60
@@ -10,11 +11,37 @@ paddle = pg.Rect(WIDTH // 2 - paddle_w // 2,
                  HEIGHT - 10 -paddle_h,
                  paddle_w, paddle_h)
 
+ball_radius = 25
+ball_speed = 3
+ball_rect = int(ball_radius * 2 ** 0.5)
+ball = pg.Rect(randrange(ball_rect, WIDTH - ball_rect), HEIGHT // 2, ball_rect, ball_rect)
+dx, dy = 1, -1
+
+block_list = [pg.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
+colours = [(73, 188, 12), (30, 142, 234), (238, 169, 26)]
+color_list = [colours[randrange(0, 3)] for i in range(10) for j in range(4)]
 
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
 background = pg.image.load('background.jpg').convert()
+
+def detect_collision(dx, dy, ball, rect):
+    if dx > 0:
+        delta_x = ball.right - rect.left
+    else:
+        delta_x = rect.right - ball.left
+    if dy > 0:
+        delta_y = ball.bottom - rect.top
+    else:
+        delta_y = rect.bottom - ball.top
+    if abs(delta_x - delta_y) < 10:
+        dx, dy = -dx, -dy
+    elif delta_x > delta_y:
+        dy = -dy
+    elif delta_y > delta_x:
+        dx = -dx
+    return dx, dy
 
 run = True
 
@@ -22,11 +49,33 @@ while run:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-        if pg.key.get_pressed()[pg.K_RIGHT] and paddle.right <= WIDTH:
-            paddle.left += paddle_speed
-        if pg.key.get_pressed()[pg.K_LEFT] and paddle.left >= 0:
-            paddle.right -= paddle_speed
+    key = pg.key.get_pressed()
+    if key[pg.K_LEFT] and paddle.left > 0:
+        paddle.left -= paddle_speed
+    if key[pg.K_RIGHT] and paddle.right < WIDTH:
+        paddle.right += paddle_speed
     screen.blit(background, (0, 0))
+
+    hit_index = ball.collidelist(block_list)
+    if hit_index != -1:
+        hit_rect = block_list.pop(hit_index)
+        hit_color = color_list.pop(hit_index)
+        dx, dy = detect_collision(dx, dy, ball, hit_rect)
+        hit_rect.inflate_ip(ball.width * 3, ball.height * 3)
+        pg.draw.rect(screen, hit_color, hit_rect)
+        fps += 2
+
+    [pg.draw.rect(screen, color_list[color], block) for color, block in enumerate(block_list)]
     pg.draw.rect(screen, pg.Color('yellow'), paddle)
+    pg.draw.circle(screen, pg.Color('white'), ball.center, ball_radius)
+    ball.x += ball_speed * dx
+    ball.y += ball_speed * dy
+    if ball.centerx < ball_radius or ball.centerx > WIDTH - ball_radius:
+        dx = -dx
+    if ball.centery < ball_radius:
+        dy = -dy
+    if ball.colliderect(paddle) and dy > 0:
+        dx, dy = detect_collision(dx, dy, ball, paddle)
+
     pg.display.flip()
     clock.tick(fps)
