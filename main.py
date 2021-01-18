2abef37da87203c3a98c838334d0e9f7202f696f
Tyzzko <70973519+Tyzzko@users.noreply.github.com>
2021-01-18 13:08:22 +0300

    ALL FUCKING BULLSHIT

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 from random import randrange
 
-WIDTH, HEIGHT = 1211, 723
+WIDTH, HEIGHT = 1211, 783
 fps = 60
 
 paddle_w = 340
@@ -17,7 +17,9 @@ ball_rect = int(ball_radius * 2 ** 0.5)
 ball = pg.Rect(randrange(ball_rect, WIDTH - ball_rect), HEIGHT // 2, ball_rect, ball_rect)
 dx, dy = 1, -1
 
-block_list = [pg.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
+# lives = [pg.Rect(10 + 60 * i, 5, 60, 60) for i in range(3)]
+lives = 3
+block_list = [pg.Rect(10 + 120 * i, 60 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
 colours = [(73, 188, 12), (30, 142, 234), (238, 169, 26)]
 color_list = [colours[randrange(0, 3)] for i in range(10) for j in range(4)]
 text1 = ''
@@ -82,15 +84,20 @@ while run:
             fps += 2
 
     [pg.draw.rect(screen, color_list[color], block) for color, block in enumerate(block_list)]
+    [pg.draw.circle(screen, (255, 0, 0), (40 + 70 * i, 30), 30) for i in range(lives)]
     pg.draw.rect(screen, pg.Color('yellow'), paddle)
     pg.draw.circle(screen, pg.Color('white'), ball.center, ball_radius)
     ball.x += ball_speed * dx
     ball.y += ball_speed * dy
 
     if ball.bottom > HEIGHT:
-        block_list, color_list = [], []
-        text1 = pg.font.Font(None, 120).render('GAME OVER!', True,
-                          (255, 255, 255))
+        if lives > 0:
+            lives -= 1
+            ball.left, ball.top = 600, 450
+        else:
+            block_list, color_list = [], []
+            text1 = pg.font.Font(None, 120).render('GAME OVER!', True,
+                              (255, 255, 255))
     elif not len(block_list):
         block_list, color_list = [], []
         text1 = pg.font.Font(None, 120).render('YOU WIN!', True,
